460f61c499b7b47135dd9939dd41f1ffe37e76e5
frappeerpnext <frapperpnext@gmail.com>
2022-10-26 12:35:59 +0700

    update sxale_invoice

--- erpnext/accounts/doctype/sales_invoice/sales_invoice_dashboard.py
+++ erpnext/accounts/doctype/sales_invoice/sales_invoice_dashboard.py
@@ -10,12 +10,10 @@ def get_data():
 			"Payment Entry": "reference_name",
 			"Payment Request": "reference_name",
 			"Sales Invoice": "return_against",
-			"Auto Repeat": "reference_document",
 			"Purchase Invoice": "inter_company_invoice_reference",
 		},
 		"internal_links": {
 			"Sales Order": ["items", "sales_order"],
-			"Timesheet": ["timesheets", "time_sheet"],
 		},
 		"transactions": [
 			{
@@ -24,13 +22,10 @@ def get_data():
 					"Payment Entry",
 					"Payment Request",
 					"Journal Entry",
-					"Invoice Discounting",
-					"Dunning",
 				],
 			},
-			{"label": _("Reference"), "items": ["Timesheet", "Delivery Note", "Sales Order"]},
+			{"label": _("Reference"), "items": [ "Delivery Note", "Sales Order"]},
 			{"label": _("Returns"), "items": ["Sales Invoice"]},
-			{"label": _("Subscription"), "items": ["Auto Repeat"]},
 			
 		],
 	}
