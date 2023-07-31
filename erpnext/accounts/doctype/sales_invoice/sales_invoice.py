fe3ba4596b50a4a8d98dc9613d8db0a33725cf82
frappeerpnext <frapperpnext@gmail.com>
2023-07-31 08:02:10 +0000

    update 2023-07-31 3:02 Pm

--- erpnext/accounts/doctype/sales_invoice/sales_invoice.py
+++ erpnext/accounts/doctype/sales_invoice/sales_invoice.py
@@ -7,6 +7,7 @@ from frappe import _, msgprint, throw
 from frappe.contacts.doctype.address.address import get_address_display
 from frappe.model.mapper import get_mapped_doc
 from frappe.model.utils import get_fetch_values
+from frappe.model.naming import make_autoname
 from frappe.utils import (
 	add_days,
 	add_months,
@@ -248,7 +249,10 @@ class SalesInvoice(SellingController):
 		self.calculate_taxes_and_totals()
 
 	def before_save(self):
+		# if not self.pos_profile and self.is_new():
+		self.document_number = make_autoname(self.document_number_prefix + ".#####", "", self)
 		set_account_for_mode_of_payment(self)
+		
 		# frappe.enqueue('erpnext.accounts.doctype.sales_invoice.sales_invoice.update_price_list_rate', self=self)
 	
 
